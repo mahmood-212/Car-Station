a85faf6ff58c9e6a7787ec425e5838aa7574f7ac
Your Name <you@example.com>
2022-05-16 15:55:51 +0200

    add bootstrap form and add static and use bootstrap in accounts

--- car_station_pro/settings.py
+++ car_station_pro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "bootstrap4",
     'car_station_app',
     'accounts',
     'company',
@@ -124,7 +125,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+# MEDIA_ROOT = [
+#     BASE_DIR / "media",
+#     '/var/www/media/',
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# LOGOUT_REDIRECT_URL = ''
+# LOGIN_REDIRECT_URL = ''
+LOGIN_URL = 'accounts:login'
