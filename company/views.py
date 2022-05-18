0710569cefd6b276bae604bb68ba937825e59c5c
Your Name <you@example.com>
2022-05-18 10:47:22 +0200

    all comapny branches

--- company/views.py
+++ company/views.py
@@ -3,11 +3,16 @@ from django.contrib.auth.decorators import login_required
 from .forms import Company_Form,CompanyBranch_Form
 from .models import Company,CompanyBranch
 from django.urls import reverse
+from django.core.paginator import Paginator
+
 # Create your views here.
 @login_required
 def CompanyBranchs(request):
     companybranch = CompanyBranch.objects.filter(user=request.user)
-    return render(request,'CompanyBranchs.html', {'branch':companybranch})
+    paginator = Paginator(companybranch,25)
+    page_number = request.GET.get('page')
+    page_pbj = paginator.get_page(page_number)
+    return render(request,'CompanyBranchs.html', {'branch':page_pbj})
 @login_required
 def new_Company(request):
     if request.method=='POST':
